3eb787e6f227a70c5c1cbc594a8fc3f1fadac961
Jesus <ajesus@realprotect.net>
2019-12-31 18:31:11 -0300

    Updated returned fields

--- get_windows_local_administrators/get_windows_local_admin.ps1
+++ get_windows_local_administrators/get_windows_local_admin.ps1
@@ -7,11 +7,15 @@ $inGroup=$temp2 -replace ".*\\","";
 $ad=$temp2 | Select-String ".*\\.*"; 
 $domains=$ad -replace "\\.*",""; 
 $domains=$domains -split"`n"; 
-$inGroup=$inGroup -split"`n"; 
+$inGroup=$inGroup -split"`n";
+$type=@() # Try to figure out how to define if is user or group
 [Int32]$totalValues=$($temp2).Count; 
 $csv=@()
 
 for([Int32]$i=0; $i -lt [Int32]$totalValues; $i++){
-    $csv+="`"$hostname`", `"$IP`",`"$($domains[$i])`",`"$($inGroup[$i])`"";
+    if($domains[$i] -eq ""){
+        $domains[$i]="local"
+    }
+    $csv+="`"$($inGroup[$i])`",`"$($type[$i])`",`"$($domains[$i])`",`"$hostname`", `"$IP`"";
     } 
 [System.Array]$csv[0..$($($csv.Length)-1)]
